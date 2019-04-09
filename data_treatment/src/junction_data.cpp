57f98b94ae8fdfcd2b70756b6ca871db67badc7c
Tiago Almeida <tm.almeida@ua.pt>
2019-04-09 11:52:54 +0100

    updates_probabilities

--- data_treatment/src/junction_data.cpp
+++ data_treatment/src/junction_data.cpp
@@ -25,6 +25,7 @@ using namespace cv;
 
 ros::Publisher merged_image;
 ros::Publisher intersect_image;
+ros::Publisher non_intersect_image;
 ros::Publisher prob_map;
 
 class junction_data
@@ -80,6 +81,7 @@ public:
     if (current_image_alg1 && current_image_alg2)
     {
       Mat img_diff;
+      Mat img_ninter;
       Mat img_alg1 = current_image_alg1->image;
       Mat img_alg2 = current_image_alg2->image;
       int i, j = 0;
@@ -88,13 +90,17 @@ public:
       threshold(img_alg1, img_alg1, 0, 255, THRESH_BINARY | THRESH_OTSU);
       threshold(img_alg2, img_alg2, 0, 255, THRESH_BINARY | THRESH_OTSU);
       bitwise_and(img_alg1, img_alg2, img_diff);
+       
+      bitwise_xor(img_alg1,img_alg2,img_ninter);
       auto img_final_diff = cv_bridge::CvImage{current_image_alg1->header, "mono8", img_diff};
+      auto img_final_nao_int=cv_bridge::CvImage{current_image_alg1->header, "mono8", img_ninter};
       intersect_image.publish(img_final_diff);
-      probabilitiesMapImage(img_diff);
+      non_intersect_image.publish(img_final_nao_int);
+      probabilitiesMapImage(img_diff,img_ninter);
     }
   }
 
-  void probabilitiesMapImage(Mat &input)
+  void probabilitiesMapImage(Mat &input, Mat &input2)
   {
     if (current_image_alg1 && current_image_alg2)
     {
@@ -110,8 +116,12 @@ public:
       float pix_cinzentosf = 0;
       int value_filter = 0;
       float prob = 0.0;
+      float prob_non_intersect=0.0;
+      int thresh_non_intersect=1; // valore acrescentado para termos a probabilidade das secções que nao se intersectam 
       kernel_size = 25;
       input.convertTo(input, CV_32F);
+      input2.convertTo(input2, CV_32F);
+      input2=input2/(float)(kernel_size+thresh_non_intersect);
 
       kernel = Mat::ones(kernel_size, kernel_size, CV_32F) /(float)(kernel_size * kernel_size);
 
@@ -122,23 +132,29 @@ public:
 
       filter2D(input, img_filt, ddepth, kernel, anchor, delta, BORDER_DEFAULT);
       img_final = Mat::zeros(input.rows, input.cols, CV_8UC1);
+      img_filt=img_filt+input2;
 
-      // for (x = 0; x < input.rows; x++)
-      // {
-      //   for (y = 0; y < input.cols; y++)
-      //   {
-      //     value_filter = img_filt.at<uchar>(x, y);
-      //     prob = value_filter / (float)255;
-      //     pix_cinzentosf = prob * (float)255;
-      //     img_final.at<uchar>(x, y) = (int)pix_cinzentosf;
+      for (x = 0; x < input.rows; x++)
+      {
+        for (y = 0; y < input.cols; y++)
+        {
+          value_filter = img_filt.at<uchar>(x, y);
+          prob = value_filter / (float)255;
+          pix_cinzentosf = prob * (float)255;
+          img_final.at<uchar>(x, y) = (int)pix_cinzentosf;
 
-      //     if (value_filter != 0)
-      //     {
-      //       cout << "value filter= " << value_filter << endl;
-      //       cout << "Probabilidade " << prob << endl;
-      //     }
-      //   }
-      // }
+          if (input2.at<uchar>(x, y)!=0)
+          {
+            prob_non_intersect=input2.at<uchar>(x, y)/(float) 255;
+          }
+
+          // if (value_filter != 0)
+          // {
+          //   cout << "value filter= " << value_filter << endl;
+          //   cout << "Probabilidade " << prob << endl;
+          // }
+        }
+      }
       
       img_filt.convertTo(img_filt,CV_8UC1);
       auto img_final_map = cv_bridge::CvImage{current_image_alg1->header, "mono8", img_filt};
@@ -159,6 +175,7 @@ int main(int argc, char **argv)
   image_transport::Subscriber sub_img2 = it.subscribe("data_treatment2/final", 10, &junction_data::imageAlg2, &data);
   merged_image = n.advertise<sensor_msgs::Image>("junction_data/summed_img", 10);
   intersect_image = n.advertise<sensor_msgs::Image>("junction_data/diff_img", 10);
+  non_intersect_image=n.advertise<sensor_msgs::Image>("junction_data/nonintersect_img", 10);
   prob_map = n.advertise<sensor_msgs::Image>("junction_data/prob_map", 10);
   ros::spin();
 
